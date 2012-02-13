da4ed2b5ce1f447079ed84ccc22f407743f1a9e6
Derek Eddow <DerekEddow@gmail.com>
2012-02-13 02:32:30 -0800

    Please Work

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testees...");
+            Console.WriteLine("Testees...");
+            Console.WriteLine("1, 2, 3?");
+            Console.WriteLine("HELP");
+        }
+    }
+}
